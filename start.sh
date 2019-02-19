e6ad18a3cb27a7c9759b0ff18bbd39f750b19e5e
Jared <jaredgreene1@gmail.com>
2019-02-19 16:50:58 -0500

    Serve optimized production ui on device

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 modprobe v4l2_common
 cd /usr/src/app/frontend
-npm run build && npm start &
+npm run build && ./node_modules/serve/bin/serve.js -c 10 -s ./build -p 80 &
 
 cd /usr/src/app/backend
 python3.6 -m main.py 
